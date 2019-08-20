23397d1cdfe16f73d73d3751662f9416db7d6519
DarXe <darxe@users.noreply.github.com>
2019-08-20 23:59:18 +0200

    Timer restart

--- main.cpp
+++ main.cpp
@@ -5,31 +5,27 @@
 using namespace std;
 
 void endProgram();
+void cls();
+
+HANDLE hOut(GetStdHandle(STD_OUTPUT_HANDLE));
+COORD c;
 
 int main()
 {
-	SetConsoleTitle("Timer");
-	HANDLE hOut(GetStdHandle(STD_OUTPUT_HANDLE));
-	COORD c;
+	SetConsoleTitleA("Timer");
 	clock_t t(clock());
 	unsigned short temp(0); //temp to set
-	int timer(0); //couting down
-	string title(""); //title
-	string _title(""); //title
+	int timer(0), _timer(0); //couting down
+	string title(""), _title(""); //title
 
 	//set timer
 	cout<<"Minutes: "; cin>>temp; timer+=temp*60000;
 	cout<<"Seconds: "; cin>>temp; timer+=temp*1000;
 	cout<<"Title: "; cin>>title;
 	title += ' ';
-	SetConsoleTitle(title.c_str());
+	SetConsoleTitleA(title.c_str());
 	
-	//clear
-	c.X = 0; c.Y = 0; SetConsoleCursorPosition(hOut, c);
-	for (size_t i = 0; i < 20; i++)
-	{
-		cout<<"                                                                                                                        "<<endl;
-	}
+	cls();
 	
 	//hide cursor
 	CONSOLE_CURSOR_INFO CURSOR;
@@ -37,55 +33,74 @@ int main()
 	CURSOR.bVisible = FALSE;
 	SetConsoleCursorInfo(hOut, &CURSOR);
 
-	//counting
-	while (timer>0)
-	{
-		t = clock();
-		c.X = 0; c.Y = 0; SetConsoleCursorPosition(hOut, c);
-		Sleep(111);
-		if(timer/60000<10)
-			cout<<"0";
-		cout<<timer/60000<<":";
-		if(timer/1000%60<10)
-			cout<<"0";
-		cout<<timer/1000%60<<":";
-		cout<<timer%1000<<"    "<<endl;
-
-		_title = title;
-		//int to string, minutes
-		temp = timer/60000;
-		if(temp<10)
-			title += temp+48;
-		else
-		{
-			short last(temp%10);
-			temp /= 10; temp %= 10;
-			title += temp+48; title += last+48;
-		}
-		//seconds
-		title+=':';
-		temp = timer/1000%60;
-		if(temp<10)
-		{
-			title += 48; //0
-			title += temp+48;
-		}
-		else
-		{
-			short last(temp%10);
-			temp /= 10; temp %= 10;
-			title += temp+48; title += last+48;
-		}
-
-		SetConsoleTitle(title.c_str());
-		title = _title;
-		if(kbhit()) break;
-		timer -= clock()-t;
-	}
+	_timer = timer; //temp
+	//reset
 	while(true)
 	{
-		if(kbhit()) break;
-		Beep(1500, 500);
+		//counting
+		while (timer>0)
+		{
+			t = clock();
+			c.X = 0; c.Y = 0; SetConsoleCursorPosition(hOut, c);
+
+			Sleep(111);
+			if(timer/60000<10)
+				cout<<"0";
+			cout<<timer/60000<<":";
+			if(timer/1000%60<10)
+				cout<<"0";
+			cout<<timer/1000%60<<":";
+			cout<<timer%1000<<"    "<<endl;	
+
+			_title = title; //temp
+			//int to string
+			//minutes
+			temp = timer/60000;
+			if(temp<10)
+				title += temp+48;
+			else
+			{
+				short last(temp%10);
+				temp /= 10; temp %= 10;
+				title += temp+48; title += last+48;
+			}
+			//seconds
+			title+=':';
+			temp = timer/1000%60;
+			if(temp<10)
+			{
+				title += 48; //0
+				title += temp+48;
+			}
+			else
+			{
+				short last(temp%10);
+				temp /= 10; temp %= 10;
+				title += temp+48; title += last+48;
+			}
+
+			SetConsoleTitleA(title.c_str());
+			title = _title;
+
+			if(kbhit() == 27) break;
+
+			timer -= clock()-t;
+		}
+
+		//beeps
+		while(true)
+		{
+			if(kbhit() == 27) break;
+			Beep(1500, 500);
+			cout<<"Press any key to stop . . . "<<endl;
+			if(kbhit()) {getch(); break;}
+		}
+		if(kbhit() == 27) break;
+		cls();
+		cout<<"[ESC] Exit"<<endl;
+		cout<<"[Any key] Restart"<<endl;
+		if(getch() == 27) break;
+		else timer = _timer;
 	}
 
 	endProgram();
@@ -97,5 +112,20 @@ void endProgram()
 	double t(clock()/(1000.0));
 	cout<<"\nTime: "<<t<<'s'<<endl;
 	cout<<"Press any key to continue . . . "<<endl;
+	cls();
 	getch();
+}
+
+void cls()
+{
+	c.X = 0; c.Y = 0; SetConsoleCursorPosition(hOut, c);
+	for (size_t i = 0; i < 20; i++)
+	{
+		for (size_t j = 0; j < 10; j++)
+		{
+			cout<<"ssssssssss";
+		}
+		cout<<"\n";
+	}
+	c.X = 0; c.Y = 0; SetConsoleCursorPosition(hOut, c);
 }
